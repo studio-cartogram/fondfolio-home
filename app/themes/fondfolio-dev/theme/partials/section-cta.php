d1e6b27730e060cc4dc8bded753c48650ff78d02
capreasy <fiona@studiocartogram.com>
2017-04-14 19:45:57 +0700

    more style changes

--- app/themes/fondfolio-dev/theme/partials/section-cta.php
+++ app/themes/fondfolio-dev/theme/partials/section-cta.php
@@ -11,9 +11,9 @@ echo '<section class="cta">';
 
   echo '<div class="row row--justify-center">';
 
-    echo '<div class="column column-8-laptop intro">';
+    echo '<div class="column column-10-laptop intro">';
 
-      echo '<h1 class="">' . $title . '</h1>';
+      echo '<h2 class="">' . $title . '</h2>';
 
       if ($button_link && $button_text) :
 
@@ -34,8 +34,8 @@ echo '<section class="cta">';
         echo '<div class="row">';
 
           while ( have_rows('blocks') ) : the_row();
-
-            echo '<div class="block__content">';
+ 
+            echo '<div class="block__content block column column-4-tablet column-4-laptop">';
 
               $title = get_sub_field('title');
               $description = get_sub_field('description');
