5a4d4ff83d2d44763397e493bf8392319fc712c4
capreasy <fiona@studiocartogram.com>
2017-04-14 18:51:06 +0700

    intiial styles

--- app/themes/fondfolio/header.php
+++ app/themes/fondfolio/header.php
@@ -1,57 +1,14 @@
 <?php
-global $language;
-
-$nav_primary = array(
-  'theme_location'  => 'nav_primary',
-  'container'       => false,
-  'items_wrap'      => '%3$s',
-);
-
-echo '<header role="banner" class="js-header header">';
-
-  echo '<div class="header__group ">';
-
-    echo '<a href="' . get_bloginfo('url') . '" class="logo">';
-
-    echo '<span class="visuallyhidden">' . get_bloginfo('name') . '</span>';
-
-      echo '<span class="logo__icon">';
-
-        set_query_var( 'icon', 'logo-icon' );
-
-        get_template_part('partials/icon');
-
-      echo '</span>';
-
-      echo '<span class="logo logo__word">';
-
-        set_query_var( 'icon', 'logo' );
-
-        get_template_part('partials/icon');
-
-      echo '</span>';
-
-    echo '</a>';
-
-  echo '</div>';
-
-  echo '<div class="header__group header__group--right">';
-
-    echo '<div class="js-nav-toggle hamburger nav-toggle">';
-
-      echo '<span class="line"></span>';
-      echo '<span class="line"></span>';
-      echo '<span class="line"></span>';
-
-    echo '</div>';
-
-    echo '<ul class="header__nav list list--right list--spaced-horizontal ">';
-
-      wp_nav_menu( $nav_primary );
-
-    echo '</ul>';
-
-  echo '</div>';
-
-echo '</header>';
-
+/**
+ *
+ * DEVELOPMENT MODE ONLY
+ *
+ * Includes and Runs php files directly
+ * from the dev theme to enable debugging
+ * php from within the dev theme!
+ *
+ * Run "gulp build" to generate the theme
+ * for production before deploying!
+ *
+ */
+include get_template_directory() . DIRECTORY_SEPARATOR . '../fondfolio-dev/theme/header.php';
