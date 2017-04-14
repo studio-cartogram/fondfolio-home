329d6593c8dedcd62ef9ba584cfa52e8bd834398
capreasy <fiona@studiocartogram.com>
2017-04-14 20:00:18 +0700

    centering explain content

--- app/themes/fondfolio-dev/theme/partials/section-explain.php
+++ app/themes/fondfolio-dev/theme/partials/section-explain.php
@@ -8,70 +8,78 @@ $button_link = get_sub_field('button_link');
 
 echo '<section class="explain">';
 
-  echo '<div class="row">';
+echo '<div class="row row--justify-center">';
 
-    echo '<div class="column intro">';
+  echo '<div class="column column-10-laptop intro">';
 
-      echo '<h1>' . $title . '</h1>';
+    echo '<div class="row">';
 
-      echo '<p class="secondary">' . $subtitle . '</p>';
+      echo '<div class="column intro">';
+
+        echo '<h1>' . $title . '</h1>';
+
+        echo '<p class="secondary">' . $subtitle . '</p>';
+
+      echo '</div>';
 
     echo '</div>';
+
+    if( have_rows('blocks') ):
+
+      echo '<div class="row">';
+
+        while ( have_rows('blocks') ) : the_row();
+
+          echo '<div class="block__content block column column-6-tablet column-6-laptop">';
+
+            $title = get_sub_field('title');
+            $description = get_sub_field('description');
+
+            get_template_part('partials/block-icon');
+
+            echo '<h3>' . $title. '</h3>';
+
+            echo $description;
+
+          echo '</div>';
+
+        endwhile;
+
+      echo '</div>';
+
+    endif;
+
+    if( have_rows('badges') ):
+
+      echo '<div class="row row--justify-center">';
+
+        while ( have_rows('badges') ) : the_row();
+
+          echo '<div class="badge__content block">';
+
+            $title = get_sub_field('title');
+
+            get_template_part('partials/block-icon');
+
+            echo '<h3>' . $title . '</h3>';
+
+          echo '</div>';
+
+        endwhile;
+
+      echo '</div>';
+
+    endif;
+
+    if ($button_link && $button_text) :
+
+      echo '<p class="buttons"><a href="' . $button_link .'" class="button button--primary button--large">' . $button_text . '</a></p>';
+
+    endif;
+
+   echo '</div>';
 
   echo '</div>';
-
-  if( have_rows('blocks') ):
-
-    echo '<div class="row">';
-
-      while ( have_rows('blocks') ) : the_row();
-
-        echo '<div class="block__content block column column-6-tablet column-6-laptop">';
-
-          $title = get_sub_field('title');
-          $description = get_sub_field('description');
-
-          get_template_part('partials/block-icon');
-
-          echo '<h3>' . $title. '</h3>';
-
-          echo $description;
-
-        echo '</div>';
-
-      endwhile;
-
-    echo '</div>';
-
-  endif;
-
-  if( have_rows('badges') ):
-
-    echo '<div class="row">';
-
-      while ( have_rows('badges') ) : the_row();
-
-        echo '<div class="badge__content block">';
-
-          $title = get_sub_field('title');
-
-          get_template_part('partials/block-icon');
-
-          echo '<h3>' . $title . '</h3>';
-
-        echo '</div>';
-
-      endwhile;
-
-    echo '</div>';
-
-  endif;
-
-  if ($button_link && $button_text) :
-
-    echo '<p class="buttons"><a href="' . $button_link .'" class="button button--primary button--large">' . $button_text . '</a></p>';
-
-  endif;
 
 echo '</section>';
 
