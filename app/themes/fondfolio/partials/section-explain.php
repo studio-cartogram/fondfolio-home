d17f53710d71c7de3f9e2d90257fdd8c2dd9137b
Matt Seccafien <matt@studiocartogram.com>
2017-05-21 11:19:42 -0400

    centerd blocks

--- app/themes/fondfolio/partials/section-explain.php
+++ app/themes/fondfolio/partials/section-explain.php
@@ -23,6 +23,8 @@ echo '<section class="' . $section . '">';
 
     if( have_rows('blocks') ):
 
+      echo '<div class="row">';
+
       echo '<div class="row blocks--' . $section . '">';
 
         while ( have_rows('blocks') ) : the_row();
@@ -50,6 +52,7 @@ echo '<section class="' . $section . '">';
 
         endwhile;
 
+      echo '</div>';
       echo '</div>';
 
     endif;
