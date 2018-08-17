f561dfe20c5167a33ca828b117e8b8150611a79e
feeohnah <fiona@studiocartogram.com>
2018-08-17 16:47:35 -0400

    fixing smooch

--- app/themes/fondfolio/partials/foot.php
+++ app/themes/fondfolio/partials/foot.php
@@ -1,5 +1,10 @@
 <?php 
     wp_footer();
+    echo '<script>
+    Smooch.init({appId: \'58ec7636c395d66a0027c37f\'}).then(function() {
+        // Your code after init is complete
+    });
+</script>';
     echo '</body>';
 
 echo '</html>';
