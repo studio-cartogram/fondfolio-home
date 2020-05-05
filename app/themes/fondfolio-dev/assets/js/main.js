d738b2278231ed4cc2ddc9aeec143cd295e6dd7a
feeohnah <fiona@studiocartogram.com>
2020-05-05 17:14:52 -0400

    fixed error with instagram feed

--- app/themes/fondfolio-dev/assets/js/main.js
+++ app/themes/fondfolio-dev/assets/js/main.js
@@ -4,83 +4,87 @@
  * js chunks
  *
  */
-import 'babel-polyfill'
-import Barba from 'barba.js'
-import log from './utils/log'
-import './vendor/webpack.publicPath'
-import loadSprite from './vendor/loadSprite'
-import Instafeed from './vendor/instafeed'
-import Scroll from './scripts/Scroll'
-import Slideshow from './scripts/Slideshow'
-import Fade from './scripts/Fade'
-import removeClasses from './utils/removeClasses'
+import "babel-polyfill";
+import Barba from "barba.js";
+import log from "./utils/log";
+import "./vendor/webpack.publicPath";
+import loadSprite from "./vendor/loadSprite";
+import Instafeed from "instafeed.js";
+import Scroll from "./scripts/Scroll";
+import Slideshow from "./scripts/Slideshow";
+import Fade from "./scripts/Fade";
+import removeClasses from "./utils/removeClasses";
 // import stickybits from 'stickybits'
 
-import {
-  ACTIVE_CLASS,
-} from './config'
+import { ACTIVE_CLASS } from "./config";
 
 class App {
   constructor() {
-    this.init()
-    loadSprite()
-    // Instafeed()
-    document.body.classList.remove('js-is-loading')
-    document.body.classList.add('js-is-initialized')
-    Barba.Pjax.init()
-    Barba.Prefetch.init()
-    Barba.Pjax.getTransition = () =>  this.Transition
+    this.init();
+    loadSprite();
+    document.body.classList.remove("js-is-loading");
+    document.body.classList.add("js-is-initialized");
+    Barba.Pjax.init();
+    Barba.Prefetch.init();
+    Barba.Pjax.getTransition = () => this.Transition;
   }
 
   init = () => {
-    this.scroll = new Scroll()
-    this.fade = new Fade()
-    this.slideshow = new Slideshow()
-    // stickybits('.js-sticky', {useStickyClasses: true})
-    this.initTransitions()
-    Barba.Dispatcher.on('initStateChange', () => {
-      document.body.classList.add('js-is-loading')
-      document.body.classList.remove('js-is-leaving')
-    })
-    Barba.Dispatcher.on('linkClicked', el => {
-      const xs = document.getElementsByClassName(ACTIVE_CLASS)
+    this.scroll = new Scroll();
+    this.fade = new Fade();
+    this.slideshow = new Slideshow();
+    this.initTransitions();
+    initInstagram();
+    Barba.Dispatcher.on("initStateChange", () => {
+      document.body.classList.add("js-is-loading");
+      document.body.classList.remove("js-is-leaving");
+    });
+    Barba.Dispatcher.on("linkClicked", (el) => {
+      const xs = document.getElementsByClassName(ACTIVE_CLASS);
       if (xs.length > 0) {
-        removeClasses(xs, ACTIVE_CLASS)
+        removeClasses(xs, ACTIVE_CLASS);
       }
       el.classList.add(ACTIVE_CLASS);
-    })
-    Barba.Dispatcher.on('transitionCompleted', (currentStatus, prevStatus) => {
-      document.body.classList.remove('js-is-loading')
-      document.body.classList.remove('js-is-leaving')
-    })
-  }
+    });
+    Barba.Dispatcher.on("transitionCompleted", (currentStatus, prevStatus) => {
+      document.body.classList.remove("js-is-loading");
+      document.body.classList.remove("js-is-leaving");
+    });
+  };
 
   initTransitions = () => {
-    const _scrollTop = this.scroll.scrollTop.bind(this)
-    // const _fadeOut = this.fade.fadeOut.bind(this)
-    // const _fadeIn = this.fade.fadeIn.bind(this)
+    const _scrollTop = this.scroll.scrollTop.bind(this);
 
     this.Transition = Barba.BaseTransition.extend({
       start() {
-        Promise
-        .all([
-          // _fadeOut(this.oldContainer).finished,
-          this.newContainerLoading,
-          _scrollTop().finished,
-        ])
-        .then(this.showNewPage.bind(this))
+        Promise.all([this.newContainerLoading, _scrollTop().finished]).then(
+          this.showNewPage.bind(this)
+        );
       },
 
       showNewPage() {
-        // _fadeIn(this.newContainer)
-        this.done()
+        this.done();
       },
-    })
-  }
-
+    });
+  };
 }
 
-const app = new App()
+const initInstagram = () => {
+  if (!document.querySelector("#instafeed")) {
+    return null;
+  }
 
-window.app = app
+  console.log("before things");
+  const feed = new Instafeed({
+    get: "user",
+    limit: 6,
+    accessToken: "2949526186.81592dc.9ff406918c514411a6fe1736c2265b73",
+    userId: 2949526186,
+    resolution: "standard_resolution",
+  });
+  feed.run();
+};
 
+const app = new App();
+
+window.app = app;
